8326627f7ffa5374973e7bfdf417de39a3a84c19
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-22 22:57:49 +0600

    first commit

---
+++ e_commerce/lib/views/home_screen.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("E-commerce app")),
+    );
+  }
+}
